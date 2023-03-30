49ef2813e0799dc29aed240dfca79b110a32be03
crbednarz <cr.bednarz@gmail.com>
2023-03-29 21:15:28 -0700

    Updated home page to be a little easier to use.

--- ui/components/layout/site-nav.tsx
+++ ui/components/layout/site-nav.tsx
@@ -1,13 +1,22 @@
+import {colorToStyle, globalColors, lerpColors} from "@/lib/style-constants";
 import { SPEC_BY_CLASS } from "@/lib/wow";
-import { Box, createStyles, Flex, MantineTheme, Menu, NavLink } from "@mantine/core";
+import { Box, Button, Card, createStyles, Flex, MantineTheme, Menu, rem, Text, Title, useMantineTheme } from "@mantine/core";
 import { useRouter } from "next/router";
 
 const useStyles = createStyles(theme => ({
   wrapper: {
-    maxWidth: '600px',
-    alignItems: 'center',
+    gap: 20,
     justifyContent: 'center',
-
+    backgroundColor: colorToStyle(globalColors.dark[8], 0.2),
+    padding: 20,
+    borderRadius: theme.radius.md,
+  },
+  title: {
+    padding: '8px 16px',
+    backgroundColor: theme.colors.dark[6],
+    borderRadius: theme.radius.sm,
+    width: '100%',
+    textAlign: 'center',
   },
   link: {
     display: 'block',
@@ -18,6 +27,18 @@ const useStyles = createStyles(theme => ({
     '&:hover': {
       backgroundColor: theme.colors.dark[6],
     },
+  },
+  card: {
+    display: 'flex',
+    gap: 5,
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: rem(200),
+    height: rem(200),
+    borderRadius: theme.radius.md,
+    '& > h3': {
+      margin: 0,
+    },
   }
 }));
 
@@ -27,41 +48,43 @@ interface SiteNavProps {
 export default function SiteNav({
 }: SiteNavProps) {
   const router = useRouter();
-  const classParam: string = (router.query.class_name ?? '') as string;
-  const specParam: string = (router.query.spec_name ?? '') as string;
-  const bracket: string = router.query.bracket as string;
 
   const { classes } = useStyles();
   
   return (
     <Flex className={classes.wrapper} wrap="wrap">
       {Object.keys(SPEC_BY_CLASS).map(wowClass => (
-        <Menu key={wowClass} trigger="hover">
-          <Menu.Target>
-            <Box className={classes.link}
-              sx={theme => ({
-                color: colorFromClass(wowClass, theme)
-              })}
-            >
-              <span>{wowClass}</span>
-            </Box>
-          </Menu.Target>
-          <Menu.Dropdown>
-            {SPEC_BY_CLASS[wowClass].map(spec => (
-              <Menu.Item
+        <Box
+          className={classes.card}
+          key={wowClass}
+        >
+          <Title
+            className={classes.title}
+            sx={theme => ({
+              color: colorFromClass(wowClass, theme)
+            })}
+            order={3}
+          >
+            {wowClass}
+          </Title>
+          {SPEC_BY_CLASS[wowClass].map(spec => (
+            <Box key={spec}>
+              <Button
                 key={spec}
+                variant="subtle"
+                color={wowClass.toLowerCase().replace(' ', '-')}
                 sx={theme => ({
                   color: colorFromClass(wowClass, theme)
                 })}
                 onClick={() => {
-                  router.push(`/${wowClass}/${spec}/${bracket ?? '3v3'}/`.replace(' ', '-'));
+                  router.push(`/${wowClass}/${spec}/${'3v3'}/`.replace(' ', '-'));
                 }}
               >
                 {spec}
-              </Menu.Item>
-            ))}
-          </Menu.Dropdown>
-        </Menu>
+              </Button>
+            </Box>
+          ))}
+        </Box>
       ))}
     </Flex>
   );
@@ -70,8 +93,3 @@ export default function SiteNav({
 function colorFromClass(wowClass: string, theme: MantineTheme) {
   return theme.colors[wowClass.toLowerCase().replace(' ', '-')];
 }
-
-function isParamMatch(name: string, param: string) {
-  return name.toLowerCase().replace(' ', '-') == param.toLowerCase();
-}
-
--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -1,6 +1,6 @@
 import Layout from "@/components/layout/layout";
 import SiteNav from "@/components/layout/site-nav";
-import { Center, Container, createStyles, Stack, Title } from "@mantine/core";
+import { Center, Container, createStyles, Stack, Title, useMantineTheme } from "@mantine/core";
 import Head from "next/head";
 
 const useStyles = createStyles(() => ({
@@ -8,15 +8,16 @@ const useStyles = createStyles(() => ({
 
 export default function Bracket() {
   const { classes } = useStyles();
+  const theme = useMantineTheme();
   return (
     <Layout>
       <Head>
         <title>Moonkin Metrics</title>
         <meta name="description" content="World of Warcraft talent explorer for rated PvP." />
       </Head>
-      <Container p='xl'>
+      <Container p="xl" size={theme.breakpoints.lg}>
         <Stack justify="center" align="center" spacing="xl">
-          <Title>Moonkin Metrics</Title>
+          <Title>Select a Specialization</Title>
           <SiteNav />
         </Stack>
       </Container>
