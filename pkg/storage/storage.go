48e49cae228c84a41b4682c0148a3ef9c1d585cd
crbednarz <cr.bednarz@gmail.com>
2023-09-19 19:57:31 -0700

    Added batch requesting to Go scanner

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -1,15 +1,30 @@
 package storage
 
 import (
-    "github.com/crbednarz/moonkinmetrics/pkg/bnet"
+	"errors"
+	"time"
+	"github.com/crbednarz/moonkinmetrics/pkg/bnet"
 )
+
+var ErrNotFound = errors.New("storage: not found")
 
 type StoredResponse struct {
     Body []byte
-    Timestamp uint64
+    Timestamp time.Time
+}
+
+type Response struct {
+    Body []byte
+    Request bnet.Request
 }
 
 type ResponseStorage interface {
+    // Stores response for later retrieval by request.
     Store(request bnet.Request, response []byte) error
+
+    // Stores set of responses, ensuring that all responses are stored or none are.
+    StoreLinked(responses []Response) error
+
+    // Retrieves response for request.
     Get(request bnet.Request) (StoredResponse, error)
 }
