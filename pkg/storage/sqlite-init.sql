c31695b93c906af4c8a02b1034307f914d9d217f
crbednarz <cr.bednarz@gmail.com>
2023-09-21 21:08:50 -0700

    Greatly simplified how credentials, regions, and locales are managed in Go scanner.

--- pkg/storage/sqlite-init.sql
+++ pkg/storage/sqlite-init.sql
@@ -2,7 +2,6 @@ CREATE TABLE IF NOT EXISTS ApiResponses(
     path TEXT NOT NULL,
     region TEXT NOT NULL,
     namespace TEXT NOT NULL,
-    locale TEXT NOT NULL,
     data BLOB NOT NULL,
     timestamp INTEGER NOT NULL,
     expires INTEGER NOT NULL,
