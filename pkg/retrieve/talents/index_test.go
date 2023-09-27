37da960d8dd70757c9ab3ed7a7ee33c0bb67b2a9
crbednarz <cr.bednarz@gmail.com>
2023-09-26 20:04:27 -0700

    Added talent tree index retrieval to Go scanner

---
+++ pkg/retrieve/talents/index_test.go
@@ -0,0 +1,100 @@
+package talents
+
+import (
+	_ "embed"
+	"io"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/crbednarz/moonkinmetrics/pkg/bnet"
+	"github.com/crbednarz/moonkinmetrics/pkg/scan"
+	"github.com/crbednarz/moonkinmetrics/pkg/storage"
+)
+
+var (
+	//go:embed testdata/valid-index.json
+	validIndex string
+
+	//go:embed testdata/bad-link-index.json
+	badLinkIndex string
+
+	//go:embed testdata/missing-data-index.json
+	missingDataIndex string
+)
+
+type MockHttpClient struct {
+	IndexBody string
+}
+
+func (m *MockHttpClient) Do(req *http.Request) (*http.Response, error) {
+	response := &http.Response{
+		StatusCode: 200,
+		Body:       io.NopCloser(strings.NewReader(m.IndexBody)),
+	}
+
+	return response, nil
+}
+
+func newMockScanner(indexBody string) (*scan.Scanner, error) {
+	client := bnet.NewClient(
+		&MockHttpClient{
+			IndexBody: indexBody,
+		},
+		"mock_client_id",
+		"mock_client_secret",
+	)
+	cache, err := storage.NewSqlite(":memory:")
+	if err != nil {
+		return nil, err
+	}
+
+	return scan.NewScanner(
+		cache,
+		client,
+	), nil
+}
+
+func TestGetTalentTreeIndex(t *testing.T) {
+	scanner, err := newMockScanner(validIndex)
+	if err != nil {
+		t.Fatalf("failed to setup scanner: %v", err)
+	}
+
+	index, err := GetTalentTreeIndex(scanner)
+	if err != nil {
+		t.Fatalf("failed to get talent tree index: %v", err)
+	}
+
+	if len(index.ClassLinks) != 15 {
+		t.Fatalf("expected 15 class links, got %d", len(index.ClassLinks))
+	}
+
+	if len(index.SpecLinks) != 40 {
+		t.Fatalf("expected 40 spec links, got %d", len(index.SpecLinks))
+	}
+}
+
+func TestTalentTreeIndexMissingDataFails(t *testing.T) {
+	scanner, err := newMockScanner(missingDataIndex)
+	if err != nil {
+		t.Fatalf("failed to setup scanner: %v", err)
+	}
+
+	_, err = GetTalentTreeIndex(scanner)
+	if err == nil {
+		t.Fatalf("expected error, got nil")
+	}
+}
+
+func TestTalentTreeIndexBadLinkFails(t *testing.T) {
+	scanner, err := newMockScanner(badLinkIndex)
+	if err != nil {
+		t.Fatalf("failed to setup scanner: %v", err)
+	}
+
+	_, err = GetTalentTreeIndex(scanner)
+	if err == nil {
+		t.Fatalf("expected error, got nil")
+	}
+}
