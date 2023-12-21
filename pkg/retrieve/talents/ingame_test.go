90a66a66cab017a91289a3d3fea9cbe8a9a918dd
crbednarz <cr.bednarz@gmail.com>
2023-12-21 11:58:09 -0800

    Go scanner now uses ingame data as a fallback for talent trees.

---
+++ pkg/retrieve/talents/ingame_test.go
@@ -0,0 +1,63 @@
+package talents
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/crbednarz/moonkinmetrics/pkg/hack"
+	"github.com/crbednarz/moonkinmetrics/pkg/retrieve/talents/testutil"
+)
+
+//go:embed testdata/valid-talent.json
+var validTalent string
+
+func TestParseIngameTalentTree(t *testing.T) {
+	scanner, err := testutil.NewMockScanner(func(requestPath string) (string, bool) {
+		if strings.HasPrefix(requestPath, "/data/wow/talent/") {
+			id := strings.TrimPrefix(requestPath, "/data/wow/talent/")
+			return strings.ReplaceAll(validTalent, "108105", id), true
+		}
+		return "", false
+	})
+	if err != nil {
+		t.Fatalf("failed to setup scanner: %v", err)
+	}
+
+	ingameTrees := hack.GetIngameTrees()
+
+	for _, ingameTree := range ingameTrees {
+		name := fmt.Sprintf("%s %s", ingameTree.ClassName, ingameTree.SpecName)
+		t.Run(name, func(t *testing.T) {
+			tree, err := talentTreeFromIngame(scanner, ingameTree)
+			if err != nil {
+				t.Fatalf("failed to parse talent tree: %v", err)
+			}
+
+			if tree.ClassName != ingameTree.ClassName {
+				t.Errorf("expected class name %s, got %s", ingameTree.ClassName, tree.ClassName)
+			}
+			
+			if tree.SpecName != ingameTree.SpecName {
+				t.Errorf("expected spec name %s, got %s", ingameTree.SpecName, tree.SpecName)
+			}
+			
+			if tree.SpecId != ingameTree.SpecId {
+				t.Errorf("expected spec id %d, got %d", ingameTree.SpecId, tree.SpecId)
+			}
+			
+			if tree.ClassId != ingameTree.ClassId {
+				t.Errorf("expected class id %d, got %d", ingameTree.ClassId, tree.ClassId)
+			}
+
+			if len(tree.PvpTalents) != 0 {
+				t.Errorf("expected pvp talents 0, got %d", len(tree.PvpTalents))
+			}
+			
+			if len(tree.ClassNodes) + len(tree.SpecNodes) != len(ingameTree.Nodes) {
+				t.Errorf("expected %d nodes, got %d", len(ingameTree.Nodes), len(tree.ClassNodes) + len(tree.SpecNodes))
+			}
+		})
+	}
+}
--- pkg/retrieve/talents/talents.go
+++ pkg/retrieve/talents/talents.go
@@ -1,57 +1,75 @@
 package talents
 
 import (
+	_ "embed"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/crbednarz/moonkinmetrics/pkg/bnet"
+	"github.com/crbednarz/moonkinmetrics/pkg/hack"
 	"github.com/crbednarz/moonkinmetrics/pkg/scan"
+	"github.com/crbednarz/moonkinmetrics/pkg/validate"
 	"github.com/crbednarz/moonkinmetrics/pkg/wow"
 )
 
+//go:embed schema/talent-tree.schema.json
+var talentTreeSchema string
+
+// GetTalentTreeIndex retrieves the full talent tree of each spec.
+// If the Battle.net API is missing a spec, fallback mechanisms will be
+// used to retrieve the talent tree, though some information may be missing.
 func GetTalentTrees(scanner *scan.Scanner) ([]wow.TalentTree, error) {
 	index, err := GetTalentTreeIndex(scanner)
 	if err != nil {
 		return nil, err
 	}
 
-	numTrees := len(index.SpecLinks)
-	requests := make(chan scan.RefreshRequest, numTrees)
-	results := make(chan scan.RefreshResult, numTrees)
-
-	scanner.Refresh(requests, results)
+	// If we've already retrieved the ingame representation of a spec, we should
+	// not retrieve it again from the Battle.net API.
+	ingameTrees := hack.GetIngameTrees()
+	specLinks := make([]SpecTreeLink, 0, len(index.SpecLinks))
 	for _, specLink := range index.SpecLinks {
-		apiRequest, err := bnet.RequestFromUrl(specLink.Url)
+		found := false
+		for _, ingameTree := range ingameTrees {
+			if ingameTree.SpecId == specLink.SpecId {
+				found = true
+				break
+			}
+		}
+		if !found {
+			specLinks = append(specLinks, specLink)
+		}
+	}
+
+	trees, err := getTreesFromSpecTrees(scanner, specLinks)
+	if err != nil {
+		return nil, err
+	}
+
+	// If the Battle.net API is missing a spec, fallback to the ingame talent tree.
+	for _, ingameTree := range ingameTrees {
+		log.Printf("retrieving talent tree from ingame data: %v - %v", ingameTree.ClassName, ingameTree.SpecName)
+		tree, err := talentTreeFromIngame(scanner, ingameTree)
 		if err != nil {
 			return nil, err
 		}
-
-		requests <- scan.RefreshRequest{
-			Lifespan:   time.Hour,
-			ApiRequest: apiRequest,
-			Validator:  nil,
-		}
-	}
-	close(requests)
-
-	trees := make([]wow.TalentTree, numTrees)
-	for i := 0; i < numTrees; i++ {
-		result := <-results
-		if result.Error != nil {
-			return nil, fmt.Errorf("failed to retrieve talent tree: %v", result.Error)
-		}
-
-		tree, err := parseTalentTreeJson(result.Body)
-		if err != nil {
-			return nil, fmt.Errorf("failed to parse talent tree json: %v", err)
-		}
-
-		trees[i] = tree
+		trees = append(trees, tree)
 	}
 
+	log.Printf("retrieving pvp talents")
+	err = attachPvpTalents(scanner, trees)
+	if err != nil {
+		return nil, err
+	}
+
+	return trees, nil
+}
+
+func attachPvpTalents(scanner *scan.Scanner, trees []wow.TalentTree) error {
 	pvpTalents, err := GetPvpTalents(scanner)
 	if err != nil {
-		return nil, fmt.Errorf("failed to retrieve pvp talents: %v", err)
+		return fmt.Errorf("failed to retrieve pvp talents: %v", err)
 	}
 
 	for _, pvpTalent := range pvpTalents {
@@ -61,6 +79,54 @@ func GetTalentTrees(scanner *scan.Scanner) ([]wow.TalentTree, error) {
 				break
 			}
 		}
+	}
+
+	return nil
+}
+
+func getTreesFromSpecTrees(scanner *scan.Scanner, specLinks []SpecTreeLink) ([]wow.TalentTree, error) {
+	validator, err := validate.NewSchemaValidator(talentTreeSchema)
+	if err != nil {
+		return nil, fmt.Errorf("failed to setup talent tree validator: %w", err)
+	}
+
+	numTrees := len(specLinks)
+	requests := make(chan scan.RefreshRequest, numTrees)
+	results := make(chan scan.RefreshResult, numTrees)
+
+	scanner.Refresh(requests, results)
+	for _, specLink := range specLinks {
+		apiRequest, err := bnet.RequestFromUrl(specLink.Url)
+		if err != nil {
+			return nil, err
+		}
+
+		requests <- scan.RefreshRequest{
+			Lifespan:   time.Hour,
+			ApiRequest: apiRequest,
+			Validator:  validator,
+		}
+	}
+	close(requests)
+
+	trees := make([]wow.TalentTree, 0, numTrees)
+	for i := 0; i < numTrees; i++ {
+		result := <-results
+		log.Printf("retrieving talent tree: %v", result.ApiRequest.Path)
+		if result.Error != nil {
+			path := result.ApiRequest.Path
+			log.Printf("failed to retrieve talent tree (%s): %v", path, result.Error)
+			continue
+		}
+
+		tree, err := parseTalentTreeJson(result.Body)
+		if err != nil {
+			path := result.ApiRequest.Path
+			log.Printf("failed to parse talent tree json (%s): %v", path, err)
+			continue
+		}
+
+		trees = append(trees, tree)
 	}
 
 	return trees, nil
