18a966a1f8ad694a402f93b4d81a54937bce9817
crbednarz <cr.bednarz@gmail.com>
2024-09-02 20:36:44 -0700

    Added additional loadout repair for bad first class talent

--- pkg/retrieve/players/repairs.go
+++ pkg/retrieve/players/repairs.go
@@ -15,9 +15,10 @@ func (r *unusedSpecRemover) Repair(s *specializationsJson) error {
 	}
 
 	var targetSpec *specializationJson
-	for _, spec := range s.Specializations {
+	for i := range s.Specializations {
+		spec := &s.Specializations[i]
 		if spec.Specialization.Name == targetSpecName {
-			targetSpec = &spec
+			targetSpec = spec
 			break
 		}
 	}
@@ -29,8 +30,28 @@ func (r *unusedSpecRemover) Repair(s *specializationsJson) error {
 	return nil
 }
 
+// removeBadFirstTalent removes the first class talent if the spell id is 0.
+// Occasionally, the first class talent returns with no details other than an id and rank.
+// It's easier to remove the talent than to try to repair it.
+func removeBadFirstTalent(s *specializationsJson) error {
+	for specIndex := range s.Specializations {
+		spec := &s.Specializations[specIndex]
+		for loadoutIndex := range spec.Loadouts {
+			loadout := &spec.Loadouts[loadoutIndex]
+			if len(loadout.SelectedClassTalents) == 0 {
+				continue
+			}
+			if loadout.SelectedClassTalents[0].Tooltip.SpellTooltip.Spell.Id == 0 {
+				loadout.SelectedClassTalents = loadout.SelectedClassTalents[1:]
+			}
+		}
+	}
+	return nil
+}
+
 func getRepairs(config LoadoutScanOptions) []repair.Repairer[specializationsJson] {
 	return []repair.Repairer[specializationsJson]{
 		&unusedSpecRemover{OverrideSpec: config.OverrideSpec},
+		repair.NewRepair(removeBadFirstTalent),
 	}
 }
