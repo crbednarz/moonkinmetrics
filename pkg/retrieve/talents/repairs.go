63eefeded567c20800a829b62f0d1f01ca7c6deb
crbednarz <cr.bednarz@gmail.com>
2024-09-08 11:41:58 -0700

    Improved talent tree repairs to filter out any nodes without ranks or rank descriptions

--- pkg/retrieve/talents/repairs.go
+++ pkg/retrieve/talents/repairs.go
@@ -5,11 +5,25 @@ import "github.com/crbednarz/moonkinmetrics/pkg/repair"
 func getTreeRepairs() []repair.Repairer[talentTreeJson] {
 	return []repair.Repairer[talentTreeJson]{
 		repair.NewRepair(func(treeJson *talentTreeJson) error {
-			firstRank := &treeJson.ClassTalentNodes[0].Ranks[0]
-			if len(firstRank.ChoiceOfTooltips) == 0 && firstRank.Tooltip == nil {
-				treeJson.ClassTalentNodes = treeJson.ClassTalentNodes[1:]
-			}
+			treeJson.ClassTalentNodes = removeNoDescriptionTalents(treeJson.ClassTalentNodes)
+			treeJson.SpecTalentNodes = removeNoDescriptionTalents(treeJson.SpecTalentNodes)
 			return nil
 		}),
 	}
+}
+
+func removeNoDescriptionTalents(talentNodes []talentNodeJson) []talentNodeJson {
+	results := make([]talentNodeJson, 0, len(talentNodes))
+
+	for _, node := range talentNodes {
+		if len(node.Ranks) == 0 {
+			continue
+		}
+		firstRank := &node.Ranks[0]
+		if len(firstRank.ChoiceOfTooltips) > 0 || firstRank.Tooltip != nil {
+			results = append(results, node)
+		}
+	}
+
+	return results
 }
