aa527d2dc46f1ce808d4bd6397da21d197d9e77c
crbednarz <cr.bednarz@gmail.com>
2024-09-10 23:50:13 -0700

    Added realm retrieval

--- pkg/wow/seasons.go
+++ pkg/wow/seasons.go
@@ -25,6 +25,30 @@ type RealmLink struct {
 	Url  string
 }
 
+type Realm struct {
+	Name string
+	Slug string
+	Id   int
+}
+
 func (p PlayerLink) SpecializationUrl() string {
 	return fmt.Sprintf("/profile/wow/character/%s/%s/specializations", p.Realm.Slug, strings.ToLower(p.Name))
+}
+
+func (l Leaderboard) GetUniqueRealms() []RealmLink {
+	realmLinkMap := make(map[string]RealmLink)
+
+	for i := range l.Entries {
+		realm := l.Entries[i].Player.Realm
+		if _, ok := realmLinkMap[realm.Slug]; !ok {
+			realmLinkMap[realm.Slug] = realm
+		}
+	}
+
+	realmLinks := make([]RealmLink, 0, len(realmLinkMap))
+	for _, realm := range realmLinkMap {
+		realmLinks = append(realmLinks, realm)
+	}
+
+	return realmLinks
 }
