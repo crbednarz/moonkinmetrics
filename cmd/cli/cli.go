38d80604bcf60d12b8659ddb30987235b92887e1
crbednarz <cr.bednarz@gmail.com>
2024-09-14 18:19:23 -0700

    Added CLI options

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
-	"runtime/pprof"
 	"strings"
+
+	"github.com/urfave/cli/v2"
 
 	"github.com/crbednarz/moonkinmetrics/pkg/bnet"
 	"github.com/crbednarz/moonkinmetrics/pkg/retrieve/seasons"
@@ -19,77 +19,90 @@ import (
 	"github.com/crbednarz/moonkinmetrics/pkg/wow"
 )
 
-type OfflineHttpClient struct{}
-
-func (c *OfflineHttpClient) Do(req *http.Request) (*http.Response, error) {
-	response := &http.Response{
-		StatusCode: 404,
-		Body:       io.NopCloser(strings.NewReader("{}")),
-	}
-
-	return response, nil
+type bracketScanOptions struct {
+	Region    bnet.Region
+	Bracket   string
+	MinRating uint
 }
 
-func main() {
-	f, err := os.Create("./test")
+func runTalentScan(c *cli.Context) error {
+	scanner, err := buildScanner(c)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("unable to build API scanner: %w", err)
 	}
-	pprof.StartCPUProfile(f)
-	defer pprof.StopCPUProfile()
-	log.Printf("Starting up")
-
-	offline := true
-
-	var httpClient bnet.HttpClient
-	if offline {
-		httpClient = &OfflineHttpClient{}
-	} else {
-		httpClient = &http.Client{}
-	}
-	client := bnet.NewClient(
-		httpClient,
-		bnet.WithCredentials(
-			os.Getenv("WOW_CLIENT_ID"),
-			os.Getenv("WOW_CLIENT_SECRET"),
-		),
-		bnet.WithLimiter(!offline),
-	)
-
-	if !offline {
-		err := client.Authenticate()
-		if err != nil {
-			panic(fmt.Errorf("failed to authenticate: %v", err))
-		}
-		log.Printf("Authentication complete")
-	}
-
-	storage, err := storage.NewSqlite("wow.db", storage.SqliteOptions{
-		NoExpire: offline,
-	})
-	if err != nil {
-		panic(err)
-	}
-	log.Printf("Storage initialized")
-	scanner := scan.NewScanner(storage, client)
 
 	trees, err := talents.GetTalentTrees(scanner)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("unable to retrieve talent trees: %w", err)
+	}
+
+	for i := range trees {
+		tree := &trees[i]
+		err = writeTalents(tree)
+		if err != nil {
+			return fmt.Errorf("unable to write talents to file: %w", err)
+		}
 	}
 	log.Printf("Talents retrieved: %d total", len(trees))
+	return nil
+}
 
-	region := bnet.RegionUS
+func runLadderScan(c *cli.Context) error {
+	region := bnet.Region(c.String("region"))
 
-	leaderboard, err := seasons.GetCurrentLeaderboard(scanner, "3v3", region)
+	scanner, err := buildScanner(c)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("unable to build API scanner: %w", err)
+	}
+
+	trees, err := talents.GetTalentTrees(scanner)
+	if err != nil {
+		return fmt.Errorf("unable to retrieve talent trees: %w", err)
+	}
+
+	bracketArg := c.String("bracket")
+	brackets := []string{bracketArg}
+	if bracketArg == "shuffle" {
+		brackets = make([]string, 0, len(wow.SpecByClass)*3)
+		for class, specs := range wow.SpecByClass {
+			classSlug := strings.ReplaceAll(class, " ", "")
+			for _, spec := range specs {
+				specSlug := strings.ReplaceAll(spec, " ", "")
+				slug := strings.ToLower(fmt.Sprintf("shuffle-%s-%s", classSlug, specSlug))
+				brackets = append(brackets, slug)
+			}
+		}
+	}
+	for _, bracket := range brackets {
+		log.Printf("Scanning bracket: %s", bracket)
+		err = scanBracket(
+			scanner,
+			trees,
+			bracketScanOptions{
+				Region:    region,
+				Bracket:   bracket,
+				MinRating: c.Uint("min-rating"),
+			},
+		)
+		if err != nil {
+			return fmt.Errorf("failed to scan bracket %s: %w", bracket, err)
+		}
+	}
+	return nil
+}
+
+func scanBracket(scanner *scan.Scanner, trees []wow.TalentTree, options bracketScanOptions) error {
+	leaderboard, err := seasons.GetCurrentLeaderboard(scanner, options.Bracket, options.Region)
+	if err != nil {
+		return fmt.Errorf("failed to retrieve leaderboard: %w", err)
 	}
 	log.Printf("Leaderboard retrieved: %v", leaderboard)
 
+	leaderboard = leaderboard.FilterByMinRating(options.MinRating)
+
 	enrichedLeaderboards, err := site.EnrichLeaderboard(scanner, &leaderboard, trees)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("failed to enrich leaderboard: %w", err)
 	}
 
 	for i := range enrichedLeaderboards {
@@ -97,10 +110,10 @@ func main() {
 
 		data, err := serialize.ExportLeaderboardToJson(leaderboard)
 		if err != nil {
-			panic(err)
+			return fmt.Errorf("failed to serialize leaderboard: %w", err)
 		}
 
-		fileName := fmt.Sprintf("%s-%s.us.json", leaderboard.ClassName, leaderboard.SpecName)
+		fileName := fmt.Sprintf("%s-%s.%s.json", leaderboard.ClassName, leaderboard.SpecName, options.Region)
 		fileName = strings.ReplaceAll(fileName, " ", "-")
 		fileName = strings.ToLower(fileName)
 
@@ -112,35 +125,141 @@ func main() {
 		log.Printf("Exported %s", path)
 	}
 
-	for i := range trees {
-		tree := &trees[i]
-		err = writeTalents(tree)
-		if err != nil {
-			panic(err)
-		}
-	}
 	log.Printf("Exported talents to json")
+	return nil
+}
+
+func buildScanner(c *cli.Context) (*scan.Scanner, error) {
+	offline := c.Bool("offline")
+
+	var httpClient bnet.HttpClient
+	if offline {
+		httpClient = &bnet.OfflineHttpClient{}
+	} else {
+		httpClient = &http.Client{}
+	}
+	client := bnet.NewClient(
+		httpClient,
+		bnet.WithCredentials(
+			c.String("client-id"),
+			c.String("client-secret"),
+		),
+		bnet.WithLimiter(!offline),
+	)
+
+	if !offline {
+		err := client.Authenticate()
+		if err != nil {
+			return nil, fmt.Errorf("failed to authenticate: %v", err)
+		}
+		log.Printf("Authentication complete")
+	}
+
+	storage, err := storage.NewSqlite("wow.db", storage.SqliteOptions{
+		NoExpire: offline,
+	})
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("Storage initialized")
+	return scan.NewScanner(storage, client), nil
+}
+
+func main() {
+	var err error
+	/*
+		f, err := os.Create("./test")
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()*/
+	app := &cli.App{
+		Name:        "moonkinmetrics",
+		Description: "Moonkin Metrics Scanning CLI",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "client-id",
+				Usage:   "Battle.net API client ID",
+				EnvVars: []string{"WOW_CLIENT_ID"},
+			},
+			&cli.StringFlag{
+				Name:    "client-secret",
+				Usage:   "Battle.net API client secret",
+				EnvVars: []string{"WOW_CLIENT_SECRET"},
+			},
+			&cli.BoolFlag{
+				Name:  "offline",
+				Usage: "Run in offline mode",
+				Value: false,
+			},
+			&cli.PathFlag{
+				Name:  "output",
+				Usage: "Output path",
+				Value: "ui/wow/",
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name:   "talents",
+				Usage:  "Export talents to JSON",
+				Action: runTalentScan,
+			},
+			{
+				Name:   "ladder",
+				Usage:  "Export ladder to JSON",
+				Action: runLadderScan,
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:  "bracket",
+						Usage: "PvP bracket to scan",
+					},
+					&cli.StringFlag{
+						Name:  "region",
+						Usage: "Region to scan",
+					},
+					&cli.UintFlag{
+						Name:  "min-rating",
+						Usage: "Minimum rating to include",
+						Value: 1600,
+					},
+					&cli.UintFlag{
+						Name:  "max-entries",
+						Usage: "Maximum entries to include",
+						Value: 7500,
+					},
+				},
+			},
+		},
+	}
+	err = app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func writeTalents(tree *wow.TalentTree) error {
 	serializedTalents, err := serialize.ExportTalentsToJson(tree)
 	if err != nil {
-		return err
+		return fmt.Errorf("unable to serialize talents: %w", err)
 	}
 	err = os.MkdirAll("ui/wow/talents/", 0755)
 	if err != nil {
-		return err
+		return fmt.Errorf("unable to create talents directory: %w", err)
 	}
 
 	fileName := fmt.Sprintf("%s-%s.json", tree.ClassName, tree.SpecName)
 	fileName = strings.ReplaceAll(fileName, " ", "-")
 	fileName = strings.ToLower(fileName)
 
-	os.WriteFile(
+	err = os.WriteFile(
 		fmt.Sprintf("ui/wow/talents/%s", fileName),
 		serializedTalents,
 		0644,
 	)
+	if err != nil {
+		return fmt.Errorf("unable to write talents to file: %w", err)
+	}
 
 	return nil
 }
