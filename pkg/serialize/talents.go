b203a485b15cd296699418dd7b9f6097846645f3
crbednarz <cr.bednarz@gmail.com>
2024-09-22 23:10:58 -0700

    Added blitz talents

--- pkg/serialize/talents.go
+++ pkg/serialize/talents.go
@@ -9,37 +9,44 @@ import (
 type talentTreeJson struct {
 	ClassName  string           `json:"class_name"`
 	SpecName   string           `json:"spec_name"`
-	ClassId    int              `json:"class_id"`
-	SpecId     int              `json:"spec_id"`
 	ClassNodes []talentNodeJson `json:"class_nodes"`
 	SpecNodes  []talentNodeJson `json:"spec_nodes"`
 	PvpTalents []talentJson     `json:"pvp_talents"`
+	HeroTrees  []heroTreeJson   `json:"hero_trees"`
+	ClassId    int              `json:"class_id"`
+	SpecId     int              `json:"spec_id"`
+}
+
+type heroTreeJson struct {
+	Name  string           `json:"name"`
+	Nodes []talentNodeJson `json:"nodes"`
+	Id    int              `json:"id"`
 }
 
 type talentNodeJson struct {
+	Unlocks  []int        `json:"unlocks"`
+	LockedBy []int        `json:"locked_by"`
+	NodeType string       `json:"node_type"`
+	Talents  []talentJson `json:"talents"`
+	MaxRank  int          `json:"max_rank"`
 	Id       int          `json:"id"`
 	X        int          `json:"x"`
 	Y        int          `json:"y"`
 	Row      int          `json:"row"`
 	Col      int          `json:"col"`
-	Unlocks  []int        `json:"unlocks"`
-	LockedBy []int        `json:"locked_by"`
-	Talents  []talentJson `json:"talents"`
-	MaxRank  int          `json:"max_rank"`
-	NodeType string       `json:"node_type"`
 }
 
 type talentJson struct {
-	Id    int       `json:"id"`
 	Name  string    `json:"name"`
 	Icon  string    `json:"icon"`
 	Spell spellJson `json:"spell"`
+	Id    int       `json:"id"`
 }
 
 type spellJson struct {
-	Id    int        `json:"id"`
 	Name  string     `json:"name"`
 	Ranks []rankJson `json:"ranks"`
+	Id    int        `json:"id"`
 }
 
 type rankJson struct {
@@ -61,6 +68,19 @@ func ExportTalentsToJson(talents *wow.TalentTree) ([]byte, error) {
 		return nil, err
 	}
 
+	heroTrees := make([]heroTreeJson, len(talents.HeroTrees))
+	for i, heroTree := range talents.HeroTrees {
+		nodes, err := convertNodes(heroTree.Nodes)
+		if err != nil {
+			return nil, err
+		}
+		heroTrees[i] = heroTreeJson{
+			Name:  heroTree.Name,
+			Nodes: nodes,
+			Id:    heroTree.Id,
+		}
+	}
+
 	pvpTalents, err := convertTalents(talents.PvpTalents)
 	if err != nil {
 		return nil, err
@@ -74,6 +94,7 @@ func ExportTalentsToJson(talents *wow.TalentTree) ([]byte, error) {
 		ClassNodes: classNodes,
 		SpecNodes:  specNodes,
 		PvpTalents: pvpTalents,
+		HeroTrees:  heroTrees,
 	}
 	return json.MarshalIndent(tree, "", "  ")
 }
