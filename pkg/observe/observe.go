378019e8b68d16c7ca3e5681890facbca0518c8d
crbednarz <cr.bednarz@gmail.com>
2024-11-26 21:36:54 -0800

    Reduce metrics rate to 1s

--- pkg/observe/observe.go
+++ pkg/observe/observe.go
@@ -3,9 +3,11 @@ package observe
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	"go.opentelemetry.io/otel/sdk/metric"
 	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
@@ -36,7 +38,7 @@ func InitObservability(target string, ctx context.Context) (ShutdownFunc, error)
 	}
 
 	meterProvider := sdkmetric.NewMeterProvider(
-		sdkmetric.WithReader(sdkmetric.NewPeriodicReader(metricExporter)),
+		sdkmetric.WithReader(sdkmetric.NewPeriodicReader(metricExporter, metric.WithInterval(time.Second))),
 		sdkmetric.WithResource(res),
 	)
 	otel.SetMeterProvider(meterProvider)
