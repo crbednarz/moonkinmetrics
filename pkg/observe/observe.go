edb0c4d514a58d0eda748d9042f2a6100fc39614
crbednarz <cr.bednarz@gmail.com>
2024-11-29 08:34:15 -0800

    Reduced metrics rate to 5s

--- pkg/observe/observe.go
+++ pkg/observe/observe.go
@@ -8,7 +8,6 @@ import (
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
 	"go.opentelemetry.io/otel/sdk/metric"
-	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 	"google.golang.org/grpc"
@@ -37,9 +36,9 @@ func InitObservability(target string, ctx context.Context) (ShutdownFunc, error)
 		return nil, fmt.Errorf("failed to create metrics exporter: %w", err)
 	}
 
-	meterProvider := sdkmetric.NewMeterProvider(
-		sdkmetric.WithReader(sdkmetric.NewPeriodicReader(metricExporter, metric.WithInterval(time.Second))),
-		sdkmetric.WithResource(res),
+	meterProvider := metric.NewMeterProvider(
+		metric.WithReader(metric.NewPeriodicReader(metricExporter, metric.WithInterval(5*time.Second))),
+		metric.WithResource(res),
 	)
 	otel.SetMeterProvider(meterProvider)
 
